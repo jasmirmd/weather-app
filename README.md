31540bbc3639d439627622ba6a95dc6beac117df
メディナ <jsmrmdn23@gmail.com>
2024-08-02 19:42:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Webpack Template 
+Basic webpack template for simple projects, created especially for my own use.
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('node:path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const imgRule = { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' };
+
+const cssRule = { test: /\.css$/i, use: ['style-loader', 'css-loader'] };
+
+const fontRule = {
+  test: /\.(woff|woff2|eot|ttf|otf)$/i,
+  type: 'asset/resource'
+};
+
+const babelRule = {
+  test: /\.(?:js|mjs|cjs)$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: [['@babel/preset-env', { targets: 'defaults' }]]
+    }
+  }
+};
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+
+  module: {
+    rules: [cssRule, imgRule, fontRule, babelRule]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html'
+    })
+  ],
+
+  devServer: {
+    client: {
+      overlay: true
+    },
+    watchFiles: ['public/*.html'],
+    hot: true,
+    open: true,
+    port: 8080
+  }
+};
